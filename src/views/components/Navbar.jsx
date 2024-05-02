38f512ebc1545d213ef923095d5b708f84892a18
Juan Aristizabal <juanaristi2001@gmail.com>
2024-05-02 08:44:16 -0500

    Add Basic Navbar

--- src/views/components/Navbar.jsx
+++ src/views/components/Navbar.jsx
@@ -2,7 +2,18 @@ import "../../styles/navbar.scss"
 
 const Navbar = () => {
   return (
-    <div className="navbar">Navbar</div>
+    <div className="navbar">
+      <div className="wrapper">
+        <div className="search">
+
+        </div>
+        <div className="items">
+          <div className="item">
+            
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
