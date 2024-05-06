dfbca17aa9985188ec270ba5c3c1f7c55d676ec3
Juan Aristizabal <juanaristi2001@gmail.com>
2024-05-06 07:08:43 -0500

    Add Login and Authentication

--- src/App.js
+++ src/App.js
@@ -3,30 +3,108 @@ import Login from "./views/Login";
 import List from "./views/List";
 import Single from "./views/Single";
 import New from "./views/New";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { useContext } from "react";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { AuthContext } from "./context/AuthContext";
 
 function App() {
+  const { currentUser } = useContext(AuthContext);
+
+  const RequireAuth = ({ children }) => {
+    return currentUser ? children : <Navigate to="/login" />;
+  };
+
   return (
     <div className="App">
       <BrowserRouter>
         <Routes>
           <Route path="/">
-            <Route index element={<Home />} />
             <Route path="login" element={<Login />} />
+            <Route
+              index
+              element={
+                <RequireAuth>
+                  <Home />
+                </RequireAuth>
+              }
+            />
             <Route path="suppliers">
-              <Route index element={<List />} />
-              <Route path=":supplierId" element={<Single />} />
-              <Route path="new" element={<New />} />
+              <Route
+                index
+                element={
+                  <RequireAuth>
+                    <List />
+                  </RequireAuth>
+                }
+              />
+              <Route
+                path=":supplierId"
+                element={
+                  <RequireAuth>
+                    <Single />
+                  </RequireAuth>
+                }
+              />
+              <Route
+                path="new"
+                element={
+                  <RequireAuth>
+                    <New />
+                  </RequireAuth>
+                }
+              />
             </Route>
             <Route path="mechanics">
-              <Route index element={<List />} />
-              <Route path=":mechanicId" element={<Single />} />
-              <Route path="new" element={<New />} />
+              <Route
+                index
+                element={
+                  <RequireAuth>
+                    <List />
+                  </RequireAuth>
+                }
+              />
+              <Route
+                path=":mechanicId"
+                element={
+                  <RequireAuth>
+                    <Single />
+                  </RequireAuth>
+                }
+              />
+              <Route
+                path="new"
+                element={
+                  <RequireAuth>
+                    <New />
+                  </RequireAuth>
+                }
+              />
             </Route>
             <Route path="products">
-              <Route index element={<List />} />
-              <Route path=":productId" element={<Single />} />
-              <Route path="new" element={<New />} />
+              <Route
+                index
+                element={
+                  <RequireAuth>
+                    <List />
+                  </RequireAuth>
+                }
+              />
+              <Route
+                path=":productId"
+                element={
+                  <RequireAuth>
+                    <Single />
+                  </RequireAuth>
+                }
+              />
+              <Route
+                path="new"
+                element={
+                  <RequireAuth>
+                    <New />
+                  </RequireAuth>
+                }
+              />
             </Route>
           </Route>
         </Routes>
